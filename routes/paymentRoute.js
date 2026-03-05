b9018955fe368290658694c7a6f83c8d9333d64a
azizaja595e <azizaja595@gmail.com>
2026-03-05 15:33:31 +0700

    jdkasj

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,9 +1,12 @@
 import {Router} from "express";
 import {createTransaction} from "../services/midtrans.js";
 import cors from "cors";
+import { handleNotification } from "../services/midtrans.js";
 
 const router = Router();
 
 router.post("/checkout", createTransaction);
+
+router.post("/notification", handleNotification);
 
 export default router;
